2132d05008b7ed744d98e8f32763b5d022efa35b
Tom1359 <tom1359@ymail.com>
2024-09-29 09:28:12 -0400

    game window working

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ from constants import *
 
 
 def main():
-    print(f"Starting asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
 
-
-
-
+        screen.fill("black")
+        pygame.display.flip()
 
 
 
